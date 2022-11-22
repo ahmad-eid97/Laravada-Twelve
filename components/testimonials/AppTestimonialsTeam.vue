731435ba08c63ecce0932749c58cf8761f6a8657
civil.ahmad.eid <civil.ahmad.eid@gmail.com>
2022-11-23 01:30:24 +0200

    add new end points data

--- components/testimonials/AppTestimonialsTeam.vue
+++ components/testimonials/AppTestimonialsTeam.vue
@@ -7,57 +7,22 @@
                 
             </div>
             <div class="row pt-45">
-                <div class="col-lg-4 col-md-6 projects-col">
+                <div v-for="member in team.teams" :key="member.id" class="col-sm-6 col-md-4 projects-col">
                     <div class="card">
                         <div class="thumb">
-                            <img src="/assets/images/testimonials/9.jpg" alt="">
+                            <img :src="member.image" alt="">
+                            <div class="rollover"></div>
                         </div>
                         <div class="content">
-                            <h4>RICHARD SWIFT</h4>
-                            <h5>junior consultant</h5>
-                            <h6>Donec vestibulum lectus sem, vel convallis ligula commodo ac. Aenean congue
-                                placerat risus, eu ullamcorper velit maximus sed.</h6>
-                            <h5>tel. 001 234 56 789</h5>
-                            <h5>richard@your-website.com</h5>
+                            <h4>{{member.name}}</h4>
+                            <h5>{{member.job}}</h5>
+                            <h6>{{member.description}}</h6>
+                            <h5>{{member.phone}}</h5>
+                            <h5>{{member.email}}</h5>
 
                         </div>
                     </div>
                 </div>
-                
-                <div class="col-lg-4 col-md-6 projects-col">
-                    <div class="card">
-                        <div class="thumb">
-                            <img src="/assets/images/testimonials/9.jpg" alt="">
-                        </div>
-                        <div class="content">
-                            <h4>RICHARD SWIFT</h4>
-                            <h5>junior consultant</h5>
-                            <h6>Donec vestibulum lectus sem, vel convallis ligula commodo ac. Aenean congue
-                                placerat risus, eu ullamcorper velit maximus sed.</h6>
-                            <h5>tel. 001 234 56 789</h5>
-                            <h5>richard@your-website.com</h5>
-
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="col-lg-4 col-md-6 projects-col">
-                    <div class="card">
-                        <div class="thumb">
-                            <img src="/assets/images/testimonials/9.jpg" alt="">
-                        </div>
-                        <div class="content">
-                            <h4>RICHARD SWIFT</h4>
-                            <h5>junior consultant</h5>
-                            <h6>Donec vestibulum lectus sem, vel convallis ligula commodo ac. Aenean congue
-                                placerat risus, eu ullamcorper velit maximus sed.</h6>
-                            <h5>tel. 001 234 56 789</h5>
-                            <h5>richard@your-website.com</h5>
-
-                        </div>
-                    </div>
-                </div>
-                
             </div>
         </div>
     </div>
@@ -65,7 +30,8 @@
 
 <script>
 export default {
-    name: 'AppTestimonialsTeam'
+    name: 'AppTestimonialsTeam',
+    props: ["team"]
 }
 </script>
 
